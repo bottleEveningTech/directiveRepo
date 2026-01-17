c7c6d4fcca056901ea925d51974459aa9d2298d1
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-17 16:49:51 +0530

    using host element

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -7,9 +7,13 @@ import { Directive, input } from "@angular/core";
         '(click)': 'onConfirmLeavePage($event)'
     }
 })
+//this is a custom attribute directive
 export class SafeLinkDirective{
     queryParam = input('myapp', {alias: 'appSafeLink'}); //default fallback is myapp
     //this variable can also be named as appSafeLink, no problem in that to give input like appSafeLink = "something"
+
+
+    private hostEleRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
     constructor(){
         console.log('Safe link directive is active');
     }
@@ -18,8 +22,10 @@ export class SafeLinkDirective{
         const hasConfirmed = window.confirm('Do you want to leave the app');
 
         if(hasConfirmed){
-            const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            // const address = (event.target as HTMLAnchorElement).href;
+            // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            const address = this.hostEleRef.nativeElement.href;
+            this.hostEleRef.nativeElement.href = address + '?from=' + this.queryParam();
 
             return;
         }
