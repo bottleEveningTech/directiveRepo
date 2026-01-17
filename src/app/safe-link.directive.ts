3ef38270f296c668c2f3bbe42163b06445a22125
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-17 16:26:49 +0530

    acceptin inputs in directives

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -8,6 +8,8 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirective{
+    queryParam = input('myapp'); //default fallback is myapp
+
     constructor(){
         console.log('Safe link directive is active');
     }
@@ -17,7 +19,7 @@ export class SafeLinkDirective{
 
         if(hasConfirmed){
             const address = (event.target as HTMLAnchorElement).href;
-            (event.target as HTMLAnchorElement).href = address + '?from = myapp';
+            (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
 
             return;
         }
